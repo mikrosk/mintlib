f2c64b37f08b92d8146428123dd1fd8c1fd3ae21
Miro Kropacek <miro.kropacek@gmail.com>
2020-12-28 19:05:32 +0100

    .travis.yml -> c-cpp.yml

--- .github/scripts/install.sh
+++ .github/scripts/install.sh
@@ -3,11 +3,4 @@
 # -u: Treat unset variables as an error when substituting.
 # -x: Display expanded script commands
 
-if [ "${TRAVIS_PULL_REQUEST}" = "false" -a "${TRAVIS_REPO_SLUG}" = "freemint/mintlib" ]
-then
-	BINTRAY_REPO="travis" SYSROOT_DIR="/" ./.travis/install_bintray.sh m68k-atari-mint-binutils-gdb m68k-atari-mint-gcc mintbin
-else
-	sudo add-apt-repository -y ppa:vriviere/ppa
-	sudo apt-get update
-	sudo apt-get install binutils-m68k-atari-mint gcc-m68k-atari-mint mintbin-m68k-atari-mint
-fi
+BINTRAY_REPO="travis" SYSROOT_DIR="/" ${SCRIPTS_DIR}/install_bintray.sh m68k-atari-mint-binutils-gdb m68k-atari-mint-gcc mintbin
